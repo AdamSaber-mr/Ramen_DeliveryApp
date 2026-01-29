97ca47be7644d33c953e30ad5b3fb1797b2d5d92
Adam Saber <102896@glr.nl>
2026-01-29 11:13:49 +0100

    Made the website online available

--- app/config/database.php
+++ app/config/database.php
@@ -1,21 +1,17 @@
 <?php
-// ==== LOCAL MAMP SETTINGS ====
 $local = [
     "host" => "localhost",
-    "port" => "8889",
     "dbname" => "Yume_ramen_bp02",
-    "user" => "root",
-    "pass" => "root"
+    "user" => "bp02_102896",
+    "pass" => "Adamsaber0182"
 ];
-
 try {
-    $dsn = "mysql:host={$local['host']};port={$local['port']};dbname={$local['dbname']};charset=utf8mb4";
+    $dsn = "mysql:host={$local['host']};dbname={$local['dbname']};charset=utf8mb4";
 
     $pdo = new PDO($dsn, $local['user'], $local['pass']);
 
     // Fouten als exceptions tonen (handig tijdens development)
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 } catch (PDOException $e) {
     // Stop alles als de connectie faalt
     die("Database connectie mislukt: " . $e->getMessage());
